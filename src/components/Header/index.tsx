b8ef027e967dcf59943e6b45e657f1183376e42b
magick123 <stormsongwow@gmail.com>
2021-08-06 14:21:04 +1000

    e

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -21,12 +21,12 @@ import Web3Status from '../Web3Status'
 const Nav: React.FC = () => {
   return (
     <StyledNav>
-      <StyledAbsoluteLink href="https://bscbonus.com" target="_blank" >Welcome bonus</StyledAbsoluteLink>
-      <StyledAbsoluteLink href="https://trade.bscswap.com/#/swap" className="active">Trade</StyledAbsoluteLink>
+      <StyledAbsoluteLink href="https://Babywolfcoin.com" target="_blank" >Babywolfcoin</StyledAbsoluteLink>
+      <StyledAbsoluteLink href="/#/swap" className="active">Trade</StyledAbsoluteLink>
       <StyledAbsoluteLink href="https://bscswap.info" target="_blank">Analysis</StyledAbsoluteLink>
       {/* <StyledAbsoluteLink href="https://bscswap.pro" target="_blank">BSCswap Pro</StyledAbsoluteLink> */}
       {/* <StyledAbsoluteLink href="https://stable.bscswap.com" target="_blank">BSCswap Stable</StyledAbsoluteLink> */}
-      <StyledAbsoluteLink href="https://degenswap.io" target="_blank">DegenSwap</StyledAbsoluteLink>
+      <StyledAbsoluteLink href="https://social.babywolfcoin.com" target="_blank">CryptoBook</StyledAbsoluteLink>
       <StyledAbsoluteLink href="https://pool.bscswap.com" target="_blank">LaunchField</StyledAbsoluteLink>
     </StyledNav>
   )
